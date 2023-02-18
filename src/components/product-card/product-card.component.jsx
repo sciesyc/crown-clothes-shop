1677a87a2ac404ff99408225b46c23b5a637cc27
stseshyts <istseshyts@gmail.com>
2023-02-18 13:23:31 +0300

    rewrited the product-card.component styles with styled components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -3,7 +3,12 @@ import { useContext } from 'react';
 import Button from '../button/button.component';
 import { CartContext } from '../../context/cart.context';
 
-import './product-card.styles.scss';
+import {
+  ProductCardContainer,
+  Footer,
+  Name,
+  Price,
+} from './product-card.styles.jsx';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -15,16 +20,16 @@ const ProductCard = ({ product }) => {
   };
 
   return (
-    <div className="product-card-container">
+    <ProductCardContainer>
       <img src={`${imageUrl}`} alt={`${name}`} />
-      <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button buttonType="inverted" onClick={handleProductButton}>
         Add to card
       </Button>
-    </div>
+    </ProductCardContainer>
   );
 };
 
