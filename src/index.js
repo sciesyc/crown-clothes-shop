3b89933d6874d768c7cacad465abb78624fccd62
stseshyts <istseshyts@gmail.com>
2023-06-05 17:03:30 +0300

    added redux functionality to manage categories state

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import CartProvider from './context/cart.context';
-import { CategoriesProvider } from './context/categories.context';
 import { store } from './store/store';
 // import reportWebVitals from './reportWebVitals';
 
@@ -16,11 +15,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
