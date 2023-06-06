6df1c76eb0fc67a61246e6f3a868c94bf2fb1b3b
stseshyts <istseshyts@gmail.com>
2023-06-06 15:21:20 +0300

    changed the logic of working with category reducer

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action.js';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
@@ -12,8 +12,8 @@ const Shop = () => {
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoriesArray));
     };
     getCategoriesMap();
   }, []);
