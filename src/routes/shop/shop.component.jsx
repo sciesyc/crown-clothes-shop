02b320338710b66d691498f502ff48d933587f41
stseshyts <istseshyts@gmail.com>
2023-06-10 17:44:54 +0300

    added redux-thunk and changed the logic of managing the category reducer

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,8 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js';
-import { setCategories } from '../../store/categories/category.action.js';
+import { fetchCategoriesAsync } from '../../store/categories/category.action.js';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
@@ -11,11 +10,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import logger from 'redux-logger';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -13,9 +14,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const midleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-);
+const midleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
