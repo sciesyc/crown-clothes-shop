3c939eb7592c6498e52da0b9290767c3f1f7caa9
stseshyts <istseshyts@gmail.com>
2023-06-21 16:24:07 +0300

    added files from dev branch

---
+++ src/store/store.js
@@ -0,0 +1,38 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import logger from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './root-saga';
+
+import { rootReducer } from './root-reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const midleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancer = composeEnhancer(applyMiddleware(...midleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancer);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
